8954d7cb62b64c1e2eede4ce6ec93d76acc2ab46
Dominic <dominic.m.chu@gmail.com>
2019-08-24 03:41:05 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,8 +1,14 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function () {
+    alert("Hey!")
+  })
+}
+
 
 $(document).ready(function(){
 
-function getIt() {
+//call function here
   
 }
 
